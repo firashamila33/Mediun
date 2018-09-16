3b0c9387f8ae668d7b584b9cc54a7c2f46aa1ebe
Yacine Farhat <f.yacine@bluepenlabs.com>
2018-09-16 22:59:15 +0100

    heroky deploy 32.0

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  redirectDomain: process.env.REDIRECT_DOMAIN,
-  subscriptionEndpoint: process.env.SUBSCRIPTION_ENDPOINT
+  mongoURI: "//mediun-dev:gikoinndfr14@ds131721.mlab.com:31721/mediun-dev",
+  redirectDomain: "https://tranquil-atoll-33654.herokuapp.com",
+  subscriptionEndpoint: "ws://tranquil-atoll-33654.herokuapp.com/subscriptions"
 };
