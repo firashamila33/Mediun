ab0f108f2c1ed6f2131745769723f8157791037a
Yacine Farhat <f.yacine@bluepenlabs.com>
2018-09-16 22:31:41 +0100

    heroky deploy 32.0

--- client/src/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://mediun-dev:gikoinndfr14@ds131721.mlab.com:31721/mediun-dev",
-  redirectDomain: "http://localhost:3000",
-  subscriptionsEndpoint: "ws://localhost:4002/subscriptions"
-};
