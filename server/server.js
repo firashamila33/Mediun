ed28bbaee7f8ff1aa4a49697310d04a63f1b1198
Yacine Farhat <f.yacine@bluepenlabs.com>
2018-09-16 21:26:10 +0100

    heroky deploy 26.0

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   console.log(__dirname);
   server.get("*", (req, res) => {
-    res.sendFile(path.resolve("..", "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,"..", "client", "build", "index.html"));
   });
 }
 
